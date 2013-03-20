12db0e5fe215c61ebe043b999c22a988006a47ad
Lucy Fu <lufu@redhat.com>
2013-03-20 13:22:28 -0400

    Lucy's first touch

--- test/unit/some_new_model_test.rb
+++ test/unit/some_new_model_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SomeNewModelTest < ActiveSupport::TestCase
+  test "make sure it does what it says" do
+    assert ture
+  end
+
   # test "the truth" do
   #   assert true
   # end
